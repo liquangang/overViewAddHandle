9ee8b83f51df6c66a4b7e9ad2e96a24bb3b52eae
liquangang <liquangang@idstaff.com>
2017-03-22 18:33:47 +0800

    1.提交工程

---
+++ overViewCanHandleEvent/overViewCanHandleEvent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  overViewCanHandleEvent
+//
+//  Created by biyao on 2017/3/22.
+//  Copyright © 2017年 biyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
